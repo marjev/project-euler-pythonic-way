6a99e8c3b082b967dd5ae53acc12520bc98ff9bc
Marko Jevtic <marjevtic@gmail.com>
2018-01-18 10:50:28 +0100

    Problem #1

---
+++ Problem # 1/zadatak1.py
@@ -0,0 +1,11 @@
+import itertools
+
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+suma = sum(itertools.ifilter(lambda x: x % 3 == 0 or x % 5 == 0, xrange(10)))
+print(suma)
